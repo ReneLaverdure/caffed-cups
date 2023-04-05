281f1acc9882d59aee1cc6b8baeb03388348cc1e
ReneLaverdure <renelaverdure@gmail.com>
2023-04-05 20:59:00 +1000

    capped name

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -4,7 +4,7 @@ import { useRouter } from 'next/router'
 
 import { useEffect } from 'react';
 
-export default function dashboard() {
+export default function Dashboard() {
     const session = useSession()
     const router = useRouter();
 
